7888c12f7ce940d1c2516ee59e6f121b092c323e
shingo <99462708+Shingo-Kamei@users.noreply.github.com>
2025-12-28 15:05:54 +0900

    削除機能追加

---
+++ deleteImage.php
@@ -0,0 +1,36 @@
+<?php
+
+include('./dbConfig.php');
+
+$targetDirectory = './images/';
+$imageId = $_GET['id'];
+
+if(!empty($imageId)) {
+
+    // プレースホルダーとは、SQL文の中で値を直接記述せずに、仮の値を設定する方法
+    // 「:id」はプレースホルダー(仮の値)
+    // プレースホルダーを使用することで、SQLインジェクション攻撃を防ぐことができる
+    $sql = "SELECT file_name FROM images WHERE id = :id";
+    $sth = $db->prepare($sql);
+    // bindValueメソッドは、プレースホルダーに値をバインドするためのメソッド
+    // 第一引数: プレースホルダー名
+    // 第二引数: バインドする値
+    // 第三引数: 値のデータ型(PDO::PARAM_INTは整数型を表す定数)
+    $sth->bindValue(':id', $imageId, PDO::PARAM_INT);
+    $sth->execute();
+    $getImageName = $sth->fetch();
+
+    if ($getImageName && !empty($getImageName['file_name'])) {
+
+        $deleteImage = unlink($targetDirectory . $getImageName['file_name']);
+
+        if($deleteImage) {
+            $deleteRecord = $db->prepare("DELETE FROM images WHERE id = :id");
+            $deleteRecord->bindValue(':id', $imageId, PDO::PARAM_INT);
+            $deleteRecord->execute();
+
+            header('Location: ./html/index.php', true, 303);
+            exit();
+        }
+    }
+}
